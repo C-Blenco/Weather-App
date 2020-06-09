9013c06231ea0275282853ab22d0ac2e0ee5304f
James Damatopoulos <james.damatopoulos@gmail.com>
2020-06-09 13:33:42 +1000

    Implementing buttons into Main Activity.

--- app/src/main/java/com/example/weatherapp/SplashActivity.java
+++ app/src/main/java/com/example/weatherapp/SplashActivity.java
@@ -1,13 +1,13 @@
 package com.example.weatherapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.model.LatLng;
